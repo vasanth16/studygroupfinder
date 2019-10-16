74604df2181052ee047e3259cafd70bbbd3841e9
Felix301 <49349438+Felix301@users.noreply.github.com>
2019-10-16 13:29:36 -0500

    Update groups_controller.rb

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -7,5 +7,18 @@ class GroupsController < ApplicationController
         #id = params[:id]
         @group = Group.find params[:id]
     end
+    
+    def new 
+        @group = Group.new 
+        # default: render 'new' template 
+    end  
 
+    def create
+        params.require(:group)
+        permitted = params[:group].permit(:group_id,:time,:location, :members, :class_code, :group_name)
+        @group = Group.create!(permitted)
+        flash[:notice] = "#{@group.group_name} was successfully created." 
+        redirect_to groups_path 
+    end
+    
 end
