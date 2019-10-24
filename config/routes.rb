7859641f957faa1cbe8db7199665261c33e1c0c6
Ubuntu <ubuntu@ip-172-31-28-68.us-east-2.compute.internal>
2019-10-24 21:11:49 +0000

    Added controllers and views for users and participations

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,13 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :groups 
   root :to => redirect('/groups')
+  
+  resources :users 
+  root :to => redirect('/users')
+  
+  resources :participations do
+    resources :groups
+    resources :users
+  end
+  root :to => redirect('/participations')
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_26_024257) do
+ActiveRecord::Schema.define(version: 2019_10_24_195543) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,7 +23,22 @@ ActiveRecord::Schema.define(version: 2019_09_26_024257) do
     t.string "class_code"
     t.string "group_name"
   end
-  
-  
+
+  create_table "participations", force: :cascade do |t|
+    t.bigint "group_id"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["group_id"], name: "index_participations_on_group_id"
+    t.index ["user_id"], name: "index_participations_on_user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email"
+    t.string "first_name"
+    t.string "last_name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
