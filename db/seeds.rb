039b14286a12455475dc47d9e0185e6d0e71bd0c
“Alex” <alexnbuck@gmail.com>
2019-10-30 00:46:42 +0000

    updating more seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,17 +17,16 @@ more_groups = [
     {:group_id => '6', :time => '4:30pm', :location => 'Boggs 112', :members =>'6', :class_code => 'PHYS1320', :group_name => 'Physics II'},
     {:group_id => '7', :time => '10:00am', :location => '3rd Floor Howie T', :members =>'1', :class_code => 'CMPS2100', :group_name => 'Help me please'},
     {:group_id => '8', :time => '9:30pm', :location => 'Richardson 117', :members =>'6', :class_code => 'NSCI3300', :group_name => 'Brain Buddies'},
-    {:group_id => '9', :time => '7:00pm', :location => 'ST302', :members =>'3', :class_code => 'CMPS2200', :group_name => 'Group 9'},
-    {:group_id => '10', :time => '10:30pm', :location => 'Gibson', :members =>'5', :class_code => 'CMPS1600', :group_name => 'Group 10'},
-    {:group_id => '11', :time => '9:30am', :location => 'Gibson', :members =>'14', :class_code => 'CMPS1500', :group_name => 'Group 11'},
-    {:group_id => '12', :time => '12:30pm', :location => 'ST302', :members =>'3', :class_code => 'CMPS32100', :group_name => 'Group 12'},
-    {:group_id => '13', :time => '6:00pm', :location => 'Gibson', :members =>'11', :class_code => 'CMPS1500', :group_name => 'Group 13'},
-    {:group_id => '14', :time => '9:15pm', :location => 'Gibson', :members =>'4', :class_code => 'CMPS1600', :group_name => 'Group 14'},
-    {:group_id => '15', :time => '8:30pm', :location => 'Gibson 115', :members =>'6', :class_code => 'CMPS3300', :group_name => 'Group 15'},
-    {:group_id => '16', :time => '11:00am', :location => 'Gibson', :members =>'10', :class_code => 'CMPS1600', :group_name => 'Group 16'},
-    {:group_id => '17', :time => '9:30pm', :location => 'Gibson', :members =>'3', :class_code => 'CMPS1500', :group_name => 'Group 17'},
-    {:group_id => '18', :time => '8:30pm', :location => 'ST302', :members =>'8', :class_code => 'CMPS3300', :group_name => 'Group 18'},
-    {:group_id => '19', :time => '9:30pm', :location => 'Gibson', :members =>'9', :class_code => 'CMPS1600', :group_name => 'Group 19'}
+    {:group_id => '9', :time => '7:00pm', :location => 'The Commons', :members =>'3', :class_code => 'CMPS2200', :group_name => 'Learning is fun?'},
+    {:group_id => '10', :time => '10:30pm', :location => 'Gibson 221', :members =>'5', :class_code => 'CELL1010', :group_name => 'Dr. V scares me'},
+    {:group_id => '11', :time => '9:30am', :location => '1st Floor Howie T', :members =>'14', :class_code => 'ACCN2010', :group_name => 'Accounting Study Group'},
+    {:group_id => '12', :time => '12:30pm', :location => 'Stanley Thomas 310', :members =>'3', :class_code => 'CMPS3300', :group_name => 'I am busy during Group 1 meetings'},
+    {:group_id => '13', :time => '6:00pm', :location => 'Newcomb 117', :members =>'11', :class_code => 'ECON1010', :group_name => 'Micro'},
+    {:group_id => '14', :time => '9:15pm', :location => 'Dixon 201', :members =>'4', :class_code => 'MUSC3100', :group_name => 'Learn to Sing'},
+    {:group_id => '15', :time => '8:30pm', :location => 'Gibson 115', :members =>'6', :class_code => 'MATH2210', :group_name => 'Calc III Coping Sesh'},
+    {:group_id => '16', :time => '11:00am', :location => 'Basement Howie T', :members =>'10', :class_code => 'CELL1010', :group_name => 'The mitochondria is the powerhouse of the cell'},
+    {:group_id => '18', :time => '5:30pm', :location => 'ST302', :members =>'8', :class_code => 'CMPS3300', :group_name => 'Group 18'},
+    {:group_id => '19', :time => '8:00am', :location => 'Gibson', :members =>'9', :class_code => 'CMPS1600', :group_name => 'Group 19'}
 ]
 
 more_groups.each do |group|
