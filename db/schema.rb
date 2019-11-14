ef1fd13e0ed83a5d5f0a94dcb9be3bdd81759ed7
vrajasekaran <vrajasekaran@tulane.edu>
2019-11-14 01:45:51 -0600

    Another

--- db/schema.rb
+++ db/schema.rb
@@ -42,8 +42,8 @@ ActiveRecord::Schema.define(version: 2019_11_14_051908) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    #t.index ["email"], name: "index_users_on_email", unique: true
-    #t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
