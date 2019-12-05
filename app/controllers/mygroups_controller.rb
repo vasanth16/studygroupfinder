b84c4e12ed975fec552c04615f444c2cff68f264
vrajasekaran <vrajasekaran@tulane.edu>
2019-12-05 01:09:42 -0600

    Google User dashboard works

--- app/controllers/mygroups_controller.rb
+++ app/controllers/mygroups_controller.rb
@@ -2,19 +2,23 @@ class MygroupsController < ApplicationController
 
      def index
         #@groups=Group.where(["group_name LIKE ?","%#{params[:search]}%"]).order(:group_name)
-        @mygroups = []
-        @participations= Participation.where(:user_id => 2)
-        @participations.each do |participations|
-            puts "____________"
-            @group = Group.where(:group_id => participations.group_id)
-            @group.each do |group|
-                @mygroups.append(group)
-            end
-        end
-        puts @mygroups
+        if user_signed_in? 
+            @user = current_user
         
-        @groups=@mygroups
+            @mygroups = []
+            @participations= Participation.where(:user_id => @user.id)
+            @participations.each do |participations|
+                puts "____________"
+                @group = Group.where(:group_id => participations.group_id)
+                @group.each do |group|
+                    @mygroups.append(group)
+                end
+            end
+            puts @mygroups
+            
+            @groups=@mygroups
         
         #@groups=Group.all
+        end
     end
 end
