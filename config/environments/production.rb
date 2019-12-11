0eecba00c914acb79d9598587b52151afaaa08f6
tfsmith39 <tfsmith53@gmail.com>
2019-12-11 20:34:02 +0000

    Potential fix for picture not showing up

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,6 +91,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+<<<<<<< HEAD
   config.action_mailer.delivery_method = :sendmail
 # Defaults to:
 # config.action_mailer.sendmail_settings = {
@@ -109,4 +110,9 @@ Rails.application.configure do
     :authentication => :plain,
     :enable_starttls_auto => true
   }
+=======
+  
+  config.assets.compile = true
+  config.assets.digest = true
+>>>>>>> Potential fix for picture not showing up
 end
