9773ac1418fe3603377e2562c627d363480b68ba
vrajasekaran <vrajasekaran@tulane.edu>
2019-12-11 17:40:26 -0600

    New Email config

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,11 +1,8 @@
 class UserMailer < ApplicationMailer
     def welcome_email
-        #if user_signed_in? 
-        #@user = current_user
         @user = params[:user]
         @name = params[:name]
-        @url  = 'https://study-group-finder-tulane.herokuapp.com/welcome/new#index'
+        @url  = 'https://study-group-finder-tulane.herokuapp.com/'
         mail(to: @user.email, subject: 'You have successfully joined '+@name+'!')
-        
     end
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -100,4 +100,13 @@ Rails.application.configure do
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default_options = {from: 'StudyGroupFinderApp@gmail.com'}
+  config.action_mailer.smtp_settings = {
+    :user_name => ENV['SENDGRID_USERNAME'],
+    :password => ENV['SENDGRID_PASSWORD'],
+    :domain => 'study-group-finder-tulane.herokuapp.com',
+    :address => 'smtp.sendgrid.net',
+    :port => 587,
+    :authentication => :plain,
+    :enable_starttls_auto => true
+  }
 end
