deaf7d8cf6e3acf6a1967729b1b9ddc70c5445a1
alexnbuck <ubuntu@ip-172-31-44-113.us-east-2.compute.internal>
2019-12-12 01:49:18 +0000

    Makes search work by class code

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,7 @@
 ActiveRecord::Schema.define(version: 2019_11_14_051908) do
 
   create_table "groups", force: :cascade do |t|
+    t.string "group_id"
     t.string "time"
     t.string "location"
     t.string "members"
@@ -33,13 +34,11 @@ ActiveRecord::Schema.define(version: 2019_11_14_051908) do
     t.string "email", default: "", null: false
     t.string "first_name"
     t.string "last_name"
-    t.string "google_token"
-    t.string "google_refresh_token"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "encrypted_password", default: "", null: false
-    t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
